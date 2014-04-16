e02ca7400a284a4cc6a162176d3063f99e9d2d14
Ilya Sidorenko <@iskros.(none)>
2014-04-16 21:41:11 +0400

    add input

--- js/script.js
+++ js/script.js
@@ -20,12 +20,12 @@ addEl('.friend .interests .el', 'onclick', function(){
 
 	
 	var els = document.querySelectorAll('.my .interests .el');
+
 	for (var i = 0; i < els.length; i++) {		
+
 		if (els[i].textContent === value) {
 			document.querySelector('.error-message').style.color = '#f00';
-
 			setTimeout("document.querySelector('.error-message').style.color = '#fff'", 1000)
-
 			return;
 		}
 	};
@@ -47,4 +47,27 @@ document.querySelector('.my .interests').onclick = function(e) {
     	target.parentNode.removeChild(target);
 
     return false // 
-  };
+};
+
+document.querySelector('.add').onclick = function  () {
+	
+
+	var text = document.querySelector('.new input').value;
+	document.querySelector('.new input').value = "";
+
+	var div = document.querySelector('.el').cloneNode(true)
+		els = document.querySelectorAll('.my .interests .el');
+
+	for (var i = 0; i < els.length; i++) {		
+
+		if (els[i].textContent === text) {
+			document.querySelector('.error-message').style.color = '#f00';
+			setTimeout("document.querySelector('.error-message').style.color = '#fff'", 1000)
+			return;
+		}
+	};
+
+	div.textContent = text;
+	document.querySelector('.my .interests').appendChild(div);		
+	
+};
